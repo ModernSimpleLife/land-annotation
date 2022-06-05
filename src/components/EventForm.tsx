34492e795fd65163b1019efafe28cc0fb282c15f
Lukas Herman <lherman.cs@gmail.com>
2022-06-04 21:23:44 -0400

    Update

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -6,12 +6,10 @@ import {
   IonLabel,
   IonList,
   IonListHeader,
-  IonIcon,
   //   IonDatetime,
   //   IonPopover,
 } from "@ionic/react";
 import { ChangeEvent, useEffect, useState } from "react";
-import { locate } from "ionicons/icons";
 import { Image, Location } from "../state";
 
 export interface FormEvent<T> {
@@ -69,6 +67,7 @@ export default function Form(event: FormEvent<Image>) {
           <IonLabel position="stacked">Comment</IonLabel>
           <IonInput
             value={comment}
+            disabled={!image}
             onIonChange={(e) => setComment(e.detail.value!)}
           ></IonInput>
         </IonItem>
@@ -76,12 +75,9 @@ export default function Form(event: FormEvent<Image>) {
         <IonItem>
           <IonLabel position="stacked">Location</IonLabel>
           <IonInput
+            disabled={true}
             value={location ? `${location.latitude},${location.longitude}` : ""}
-          >
-            <IonButton color="primary" onClick={getCurrentLocation}>
-              <IonIcon icon={locate}></IonIcon>
-            </IonButton>
-          </IonInput>
+          ></IonInput>
         </IonItem>
 
         {/* <IonItem>
